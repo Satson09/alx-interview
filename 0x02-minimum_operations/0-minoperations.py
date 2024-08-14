26edd41f3c3bde53eb6a203a8c6a01e59c8b3fa7
satson09 <satsunjohnson@gmail.com>
2024-08-14 18:16:03 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,15 +6,15 @@ Minimum operations
 def minOperations(n):
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
-    return operations
+
+    return minioperations
 
